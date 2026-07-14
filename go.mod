c5ac618c6abc856cdcf83f98bf77794ce9373b34
lupinus <zninggo@outlook.com>
2026-07-14 20:30:45 +0800

    bigbark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zssok/puppy
 
 go 1.26.4
+
+require github.com/zssok/dog v0.0.0-20260714122449-0f99e1a78928 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import "github.com/zssok/dog"
+
 func Bark() string {
 	return "汪!!!!!!!!!"
 }
 
 func Barks() string {
 	return "汪汪汪汪汪汪汪汪!!!!!!!!!"
+}
+
+func BigBark() string {
+
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+
+	return dog.WhenGrownUp(Barks())
 }
